cf6cc85aa56a907670ca25f70192615f7046b809
ironhead <ironhead.chuang@gmail.com>
2019-07-18 17:55:12 +0800

    correct indent for cpplint

--- vector_3d.hpp
+++ vector_3d.hpp
@@ -5,12 +5,12 @@
 
 
 class Vector3D {
-public:
+  public:
     union { float x_, r_; };
     union { float y_, g_; };
     union { float z_, b_; };
 
-public:
+  public:
     Vector3D() : x_(0.0f), y_(0.0f), z_(0.0f) {}
     Vector3D(float x, float y, float z) : x_(x), y_(y), z_(z) {}
 
